e0595cfc2424da5a33b3f35cf040953fab9bcb1f
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-02-10 19:29:16 +0300

    functional components done

--- app/todos/TodosList.tsx
+++ app/todos/TodosList.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import { Todo } from "../../typings";
+import Link from "next/link";
 
 const fetchTodos = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
   const todos: Todo[] = await res.json();
-  console.log(todos);
+
   return todos;
-  
 };
 
 async function TodosList() {
   const todos = await fetchTodos();
+  console.log(todos);
 
-  return <div>TodosList</div>;
+  return (
+    <div>
+      {todos.map((todo) => (
+        <p key={todo.id}>
+          <Link href={`/todos/${todo.id}`}>{todo.id}</Link>
+        </p>
+      ))}
+    </div>
+  );
 }
 
 export default TodosList;
--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import TodosList from './TodosList'
 
 function Todos() {
   return (
-    <div>Todos</div>
+      <div>
+    {/* @ts-ignore  */}
+          <TodosList/>
+    </div>
   )
 }
 
