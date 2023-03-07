f1f0eb76d3772f9d205e288b9b6ad0bec80de3bc
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-03-07 16:26:28 +0300

    data fetching done

--- pages/api/posts/[id].js
+++ pages/api/posts/[id].js
@@ -1,11 +1,20 @@
-import Posts from "../../../data.json"
+import Posts from "../../../data.json";
 
 export default function handlerId(req, res) {
-    const { method, cookies, query } = req 
+  const { method, cookies, query } = req;
+
+//   console.log(method);
+//   console.log(cookies);
+//   console.log(query);
+  const { id } = query;
+  console.log(id);
+
+  const post = Posts.posts.find((post) => post.id == id);
+
+    if (!post) {
+        res.status(404).json({ message: "Post not found" });
+    }
     
-    console.log(method);
-    console.log(cookies);
-    console.log(query);
-    const { id } = query
-    console.log(id);
+  res.status(200).json(post);
+ console.log(post);
 }
