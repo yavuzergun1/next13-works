bfd3c0f489eefe163f3e737462bacc919505e691
Yavuz Ergun <posta.yavuzergun@gmail.com>
2023-03-07 15:17:13 +0300

    header links added

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,8 +3,13 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="p-5 bg-blue-500 text-white font-semibold text-lg">
-      <Link href="/">Home</Link>
+    <header className=" p-5 bg-blue-500 text-white font-semibold text-lg">
+      <div className="links-container w-6/12 flex justify-between">
+        <Link href="/">Home</Link>
+        <Link href="/search">Google Search</Link>
+        <Link href="/todos">Todos App</Link>
+        <Link href="/todos">Admin</Link>
+      </div>
     </header>
   );
 }
